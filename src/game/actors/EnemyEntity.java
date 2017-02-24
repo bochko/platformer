879b490c257d40e6f7b2ac1421dbb81307d985ae
noodlesfromhell <b.atanasov@icloud.com>
2017-02-24 00:52:27 +0000

    added player, projectile, consumableEntity, enemyEntity, and the Collidable interface. Moving functionality from the game class to the Player class, so it handles itself. Currently in development : game.actors.Player.

--- src/game/actors/EnemyEntity.java
+++ src/game/actors/EnemyEntity.java
@@ -1,7 +1,18 @@
 package game.actors;
 
+import cairns.david.engine.Animation;
+import cairns.david.engine.Sprite;
+
 /**
  * Created by boyan on 23/02/17.
  */
 public class EnemyEntity extends Sprite {
+    /**
+     * Creates a new Sprite object with the specified Animation.
+     *
+     * @param anim The animation to use for the sprite.
+     */
+    public EnemyEntity(Animation anim) {
+        super(anim);
+    }
 }
